87458b36374cdd370ed05ec8ff99d4c51236490e
Sean Kim <40130483+theskim@users.noreply.github.com>
2024-02-13 23:50:43 -0500

    design mod

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>UofT Engineering Bird HSS Courses</h1>
-      <a href="https://forms.gle/ED68qWhJpU43JghN6">Request Bird Course</a>
-      <CoursesTable courses={data} />
+        <p><b>UofT</b> Engineering <b>Bird HSS</b> Courses</p>
+        <a href="https://forms.gle/ED68qWhJpU43JghN6">Request Bird Course</a>
+        <br/><br/>
+        <CoursesTable courses={data} />
     </div>
   );
 }
--- src/CourseTable.jsx
+++ src/CourseTable.jsx
@@ -4,7 +4,7 @@ const CoursesTable = ({ courses }) => {
   const [sortConfig, setSortConfig] = useState({ key: 'code', direction: 'ascending' });
   
   const [currentPage, setCurrentPage] = useState(1);
-  const coursesPerPage = 30; 
+  const coursesPerPage = 10; 
 
   const requestSort = (key) => {
     let direction = 'ascending';
@@ -40,33 +40,33 @@ const CoursesTable = ({ courses }) => {
 
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th onClick={() => requestSort('code')}>Course Code</th>
-            <th onClick={() => requestSort('title')}>Title</th>
-            <th onClick={() => requestSort('course_avg')}>Typical Avg.</th>
-            <th onClick={() => requestSort('description')}>Why this Course?</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentCourses.map((course) => (
-            <tr key={course.id}>
-              <td>{course.code}</td>
-              <td>{course.title}</td>
-              <td>{course.course_avg}</td>
-              <td>{course.description}</td>
+        <table>
+            <thead>
+            <tr>
+                <th onClick={() => requestSort('code')}>Course Code</th>
+                <th onClick={() => requestSort('title')}>Title</th>
+                <th onClick={() => requestSort('course_avg')}>Typical Avg.</th>
+                <th onClick={() => requestSort('description')}>Why this Course?</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        {Array.from({ length: pageCount }, (_, index) => (
-          <button key={index} onClick={() => setCurrentPage(index + 1)}>
-            {index + 1}
-          </button>
-        ))}
-      </div>
+            </thead>
+            <tbody>
+            {currentCourses.map((course) => (
+                <tr key={course.id}>
+                <td>{course.code}</td>
+                <td>{course.title}</td>
+                <td>{course.course_avg}</td>
+                <td>{course.description}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table> <br/>
+        <div>
+            {Array.from({ length: pageCount }, (_, index) => (
+            <button key={index} onClick={() => setCurrentPage(index + 1)}>
+                {index + 1}
+            </button>
+            ))}
+        </div>
     </>
   );
 };
