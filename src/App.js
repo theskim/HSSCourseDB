e7e7a79443525ef7774e6110c553e0806a7b6238
Sean Kim <40130483+theskim@users.noreply.github.com>
2024-02-13 23:37:27 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import CoursesTable from './CourseTable';
+import data from './courses.json';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>UofT Engineering Bird HSS Courses</h1>
+      <a href="https://forms.gle/ED68qWhJpU43JghN6">Request Bird Course</a>
+      <CoursesTable courses={data} />
     </div>
   );
 }
---
+++ src/CourseTable.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useMemo } from 'react';
+
+const CoursesTable = ({ courses }) => {
+  const [sortConfig, setSortConfig] = useState({ key: 'code', direction: 'ascending' });
+  
+  const [currentPage, setCurrentPage] = useState(1);
+  const coursesPerPage = 30; 
+
+  const requestSort = (key) => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending')
+      direction = 'descending';
+    else 
+        direction = 'ascending';
+      
+    setSortConfig({ key, direction });
+  };
+
+  const sortedCourses = useMemo(() => {
+    let sortableCourses = [...courses];
+    if (sortConfig.key !== null) {
+      sortableCourses.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) 
+          return sortConfig.direction === 'ascending' ? -1 : 1
+        if (a[sortConfig.key] > b[sortConfig.key]) 
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        return 0;
+      });
+    }
+    return sortableCourses;
+  }, [courses, sortConfig]);
+
+  const pageCount = Math.ceil(sortedCourses.length / coursesPerPage);
+
+  const currentCourses = useMemo(() => {
+    const start = (currentPage - 1) * coursesPerPage;
+    const end = start + coursesPerPage;
+    return sortedCourses.slice(start, end);
+  }, [currentPage, sortedCourses, coursesPerPage]);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => requestSort('code')}>Course Code</th>
+            <th onClick={() => requestSort('title')}>Title</th>
+            <th onClick={() => requestSort('course_avg')}>Typical Avg.</th>
+            <th onClick={() => requestSort('description')}>Why this Course?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentCourses.map((course) => (
+            <tr key={course.id}>
+              <td>{course.code}</td>
+              <td>{course.title}</td>
+              <td>{course.course_avg}</td>
+              <td>{course.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        {Array.from({ length: pageCount }, (_, index) => (
+          <button key={index} onClick={() => setCurrentPage(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default CoursesTable;
